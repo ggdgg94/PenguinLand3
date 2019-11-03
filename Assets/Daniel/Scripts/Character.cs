6f3d6996954e04863f3e07ea7046b783249b3127
ggdgg94 <ggdgg@protonmail.com>
2019-11-03 02:28:01 -0600

    Create Character base class script and set up animations and movement Create Player class and add movement

---
+++ Assets/Daniel/Scripts/Character.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+    //Related to Animations    
+    public static readonly string[,] idle = new string[,] {
+        {"IdleSW", "IdleW", "IdleNW"},
+        {"IdleS", "IdleS", "IdleN"},
+        {"IdleSE", "IdleE", "IdleNE"},
+
+    };
+
+    public static readonly string[,] move = new string[,] {
+        {"MoveSW", "MoveW", "MoveNW"},
+        {"MoveS", "MoveS", "MoveN"},
+        {"MoveSE", "MoveE", "MoveNE"},
+
+    };
+
+
+    //Related to Movement
+    public float moveSpeed;
+    public float dashSpeed;
+    public Vector3 direction;
+    public Vector3 lastDirection;
+    
+    //Related to Location
+    public float maxX, minX, maxY, minY; //To be set in Unity Editor
+
+    //Related to Animations
+    public Animator animator; 
+    public Vector3 tmpPosition;
+
+    public virtual void SetDirection(){}
+    public void Move(Vector3 dir)
+    {
+        lastDirection = dir;
+        tmpPosition = (dir.normalized * moveSpeed * Time.deltaTime) + transform.position;
+        tmpPosition.x = Mathf.Clamp(tmpPosition.x, minX, maxX);
+        tmpPosition.y = Mathf.Clamp(tmpPosition.y, minY, maxY);
+        transform.position = tmpPosition;
+    }
+
+    public void SetIdleAnimation(Vector3 dir){animator.Play(idle[(int)dir.x + 1, (int)dir.y + 1]);}
+    public void SetMoveAnimation(Vector3 dir){animator.Play(move[(int)dir.x + 1, (int)dir.y + 1]);}
+
+    
+}
---
+++ Assets/Daniel/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character 
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       SetDirection();
+       MovePlayer(); 
+    }
+
+    public override void SetDirection(){direction.Set(Mathf.RoundToInt(Input.GetAxisRaw("Horizontal")),Mathf.RoundToInt(Input.GetAxisRaw("Vertical")),0);}
+    public void MovePlayer()
+    {
+        if(direction.x == 0 && direction.y == 0)
+            SetIdleAnimation(direction);
+        else{
+            SetMoveAnimation(direction);
+            Move(direction);
+        }
+    }
+}
