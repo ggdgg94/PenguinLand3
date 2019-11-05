460b50296c52b0b817e784b5a90b0a082cf1fb23
dimas <46415558+Dimas713@users.noreply.github.com>
2019-11-05 11:40:47 -0600

    sprite and scripts
    added potion  sprite and scripts

---
+++ Assets/Dimas/scripts/scripts_for_potion/HealthPickUp.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class HealthPickUp : MonoBehaviour
+{
+    Character playerHealth;
+    public int bonusHealth = 10;
+
+    void Awake()
+    {
+        playerHealth = FindObjectOfType<Character>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(playerHealth.life < 100)
+        {
+            Destroy(gameObject);
+            playerHealth.life = playerHealth.life + bonusHealth; 
+
+        }
+    }
+
+}
