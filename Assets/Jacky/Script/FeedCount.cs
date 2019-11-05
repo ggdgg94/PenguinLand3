31617898a2daef277fd611f18333943ee6c26585
ggdgg94 <ggdgg@protonmail.com>
2019-11-05 01:37:07 -0600

    Port over more UI controls from Jacky, Fix Volume slider in Options now actually changes volume 	Menu: Remove penguins running for now until merge with my scripts

---
+++ Assets/Jacky/Script/FeedCount.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedCount : MonoBehaviour
+{
+
+    //public EnemyController feedcount;
+    
+    public int Fcount = 0;
+
+
+    void Start()
+    {
+        //feedcount = GameObject.FindGameObjectWithTag("Penguin(Clone)").GetComponent<EnemyController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Fcount = Fcount + feedcount.count;
+    }
+}
---
+++ Assets/Jacky/Script/VolumeValueChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class VolumeValueChange : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public Slider slider;
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("MusicVolume", 1f);
+    }
+    public void SetLevel()
+    {
+        float slidervalue = slider.value;
+        mixer.SetFloat("MusicVol", Mathf.Log10(slidervalue) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", slidervalue);
+    }
+}
