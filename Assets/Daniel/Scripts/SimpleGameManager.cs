56920bbbac046abfc0e44297d3158957cbf553b9
ggdgg94 <ggdgg@protonmail.com>
2019-11-26 14:11:32 -0600

    fix faulty push

--- Assets/Daniel/Scripts/SimpleGameManager.cs
+++ Assets/Daniel/Scripts/SimpleGameManager.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -310,4 +309,3 @@ public class SimpleGameManager : MonoBehaviour
 
     }
 }
->>>>>>> d8e328092404d41812adb1fccdb8bcb3671cd462
