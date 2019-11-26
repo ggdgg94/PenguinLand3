d1fbcad21005228b4d8d6433a7fcaddf5aa1a997
jchan4997 <47164259+jchan4997@users.noreply.github.com>
2019-11-25 18:24:39 -0600

    Revert "Merge branch 'master' of https://www.github.com/ggdgg94/PenguinLand3 into Jacky-last"
    This reverts commit ce82d3739340beca404354592d0fdff7e8d699c5, reversing
    changes made to 4abf00092bce87501bbf69c8d50e5d3316cf8244.

---
+++ Assets/Daniel/Scripts/SimpleGameManager.cs
@@ -0,0 +1,237 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SimpleGameManager : MonoBehaviour
+{
+
+    [Header("Characters")]
+    //Prefabs set in Editor
+    [SerializeField]
+    Player playerPrefab;
+
+    [Header("Menus")]
+    [SerializeField]
+    GameObject pauseUI;
+    bool paused = false;
+    [SerializeField]
+    GameObject loseUI;
+
+    [Header("Game UI")]
+    [SerializeField]
+    Image hearts;
+    [SerializeField]
+    Sprite[] playerLife ;
+    [SerializeField]
+    Text stageTimer;
+    static float timerTime = 60f; //for now we don't care about counting up
+    [SerializeField]
+    Text scoreText; //might get rid of this later to account for all types of penguins
+
+
+    [Header("Level Settings")]
+    //Do we want to play a level type? Keep both false for a general purpose scene
+    public bool winByTime = false;
+    public float timeForRound = 0f;
+
+    public bool winByFeed = false;
+    public int feedWinCondition = 0;
+
+
+    [Header("General Scene Settings")]
+    //General Purpose
+    public bool generateCharacter = false;
+    public bool countdownTime = false;
+
+    [Header("Advanced Scene Settings")]
+    public PenguinSpawner[] penguinSpawners;
+    
+    /* 
+    public float[] penguinSpawnTimes = new float[4];
+    public float[] itemSpawnTimes = new float[4];
+    public Vector3[] spawnPoints = new Vector3[8];
+   */
+
+    Player player; 
+    Vector3 spawnPoint = new Vector3(6,-2,0);
+    Character[] horde;
+    public static int score = 0;
+
+    public void GeneralSetup()
+    {
+        if(generateCharacter){
+            player = Instantiate(playerPrefab, spawnPoint, Quaternion.identity);
+        }else{
+            hearts.sprite = null;
+            hearts.color = new Color(255, 255, 255, 0);
+        }
+        
+    }
+
+    public void FeedSetup()
+    {
+        if(winByFeed){
+            generateCharacter = true;
+    // feedWinCondition < 0 ? feedWinCondition = 10 : feedWinCondition = feedWinCondition;
+        }
+    }
+    void Start()
+    {
+        Time.timeScale = 1f;
+        if(winByFeed){
+            GeneralSetup();
+
+        }else if(winByTime){
+
+        }else{
+            if(generateCharacter)
+                GeneralSetup();
+
+            if(countdownTime)
+                if(timeForRound <= 0){
+                    timeForRound = 60f;
+                }
+        }
+            
+
+/* 
+
+        }else{
+            Time.timeScale = 1f;
+            if(winByTime){
+                generateCharacters = true;
+                countdownTime = true;
+                SetUpPlayer();
+
+            }else if(winByFeed){
+                generateCharacters = true;
+                SetUpPlayer();
+
+            }else{
+                if(generateCharacters){
+                    SetUpPlayer();
+                    SetUpPenguinAmount(regularPenguin, 30);
+                }else{
+                    hearts.sprite = null;
+                    hearts.color = new Color(255, 255, 255, 0);
+                }
+            }
+        }
+        Debug.Log(player.minX);
+*/
+    }
+
+    void SetUpPlayer()
+    {
+        player = Instantiate(playerPrefab);
+    }
+    void SetUpPlayerPara(float x1, float x2, float y1, float y2)
+    {
+        player.minX = x1;
+        player.maxX = x2;
+        player.minY = y1;
+        player.maxY = y2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandlePause();
+        /*
+
+        if(!demo1 && !demo2 && !demo3){
+            HandlePause();
+            if(generateCharacters){
+                Spawn2();
+                UpdatePlayerLife();
+                UpdateScore();
+                CheckGameState();
+            }
+            if(countdownTime){
+                UpdateTime();
+            
+            }
+        }
+        */
+        
+        //Spawn(regularPenguin);
+
+        
+    }
+    void CheckGameState()
+    {
+        if(player.state == Character.CharacterState.Defeated){
+            loseUI.gameObject.SetActive(true);
+            Time.timeScale = 0f; //psuedo pause
+        }
+    }
+
+    public void Resume()
+    {
+        paused = false;
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Daniel-Test");
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    void HandlePause()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            paused = !paused;
+            if(paused){
+                pauseUI.SetActive(true);
+                Time.timeScale = 0f;
+            }
+        }
+    }
+    void UpdatePlayerLife()
+    {
+        hearts.sprite = playerLife[player.life];
+    }
+    void UpdateScore()
+    {
+        scoreText.text = " " + score;
+    }
+
+    void UpdateTime()
+    {
+        if(timerTime > 0){
+            timerTime -= 1 * Time.deltaTime;
+            stageTimer.text = timerTime.ToString("0");
+        }
+    }
+
+    void CheckTimedWin()
+    {
+        if(timerTime <= 0){
+            Debug.Log("You Win!");
+
+        }
+    }
+
+    void CheckScoreWin()
+    {
+        if(score >= feedWinCondition){
+
+        }
+    }
+
+    void ChangeScenes()
+    {
+
+    }
+}
